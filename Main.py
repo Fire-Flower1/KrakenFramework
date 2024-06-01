4680ef5bd7a9d222cb35725ee93d129d7c24d338
Fire-Flower1 <cohenkbruce@gmail.com>
2024-06-01 15:09:44 -0400

    added files

--- Main.py
+++ Main.py
@@ -2,3 +2,8 @@ from JsonManager import save_directory_structure_to_json
 import configparser
 import json
 
+save_directory_structure_to_json(".\\modules" "DirInfo.json")
+
+jsonFile = open("DirInfo.json")
+
+data = json.load(jsonFile)
