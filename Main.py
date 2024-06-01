189beb4b55153191c7c4cd0e769c479fd8f2cb10
Fire-Flower1 <cohenkbruce@gmail.com>
2024-06-01 15:53:21 -0400

    added files

--- Main.py
+++ Main.py
@@ -2,8 +2,17 @@ from JsonManager import save_directory_structure_to_json
 import configparser
 import json
 
-save_directory_structure_to_json(".\\modules" "DirInfo.json")
-
+save_directory_structure_to_json(".\\modules", "DirInfo.json")
+config_files = {}
 jsonFile = open("DirInfo.json")
 
 data = json.load(jsonFile)
+
+for i in data['modules']:
+    for x in range(len(data['modules'][i]['__files__'])):
+        if ".ini" in data['modules'][i]['__files__'][x]:
+            config_files = {data['modules'][i]["id-number"] : data['modules'][i]['__files__'][x]}
+
+print(config_files[0])
+
+jsonFile.close()
